41ddccca1eefb704f2f16d210785a348c5b54dec
dogukanyesilkaya <dogukanyesilkaya@outlook.com>
2026-06-25 19:19:08 +0300

    starship fix

--- modules/shell.nix
+++ modules/shell.nix
@@ -40,6 +40,44 @@
     };
     programs.starship = {
         enable = true;
-        presets = [ "pure-prompt" ];
+        enableBashIntegration = true;  # or zsh/fish
+            settings = {
+                format = ''
+                    $username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character
+                    '';
+                character = {
+                    success_symbol = "[❯](purple)";
+                    error_symbol = "[❯](red)";
+                    vimcmd_symbol = "[❮](green)";
+                };
+                directory.style = "blue";
+                git_branch = {
+                    format = "[$branch]($style)";
+                    style = "bright-black";
+                };
+                git_status = {
+                    format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+                    style = "cyan";
+                    conflicted = "​";
+                    untracked = "​";
+                    modified = "​";
+                    staged = "​";
+                    renamed = "​";
+                    deleted = "​";
+                    stashed = "≡";
+                };
+                git_state = {
+                    format = ''\([$state( $progress_current/$progress_total)]($style)\) '';
+                    style = "bright-black";
+                };
+                cmd_duration = {
+                    format = "[$duration]($style) ";
+                    style = "yellow";
+                };
+                python = {
+                    format = "[$virtualenv]($style) ";
+                    style = "bright-black";
+                };
+            };
     };
 }
