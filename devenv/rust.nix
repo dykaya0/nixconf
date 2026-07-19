3c3cb177cf479a5fedd45441fbbf092984398585
Dogukan <dogukanyesilkaya@outlook.com>
2026-07-19 14:35:27 +0300

    (devenv) rust.nix change

--- devenv/rust.nix
+++ devenv/rust.nix
@@ -3,10 +3,5 @@
 {
     languages.rust = {
         enable = true;
-        channel = "stable";
-        lsp.enable = true;
-        components = [ "rust-src" "clippy" "rustfmt" ];
     };
-
-    processes.dev.exec = "cargo run";
 }
